368ccf17d7c9e9701daac880fd38c19f7c04fd91
Rodrigo Landaeta <rlandaeta@cochlear.com>
2017-11-06 13:49:14 +1100

    Sorting fonts to make it easier to browse.

--- samples/11.typography/src/fontLookup.tsx
+++ samples/11.typography/src/fontLookup.tsx
@@ -16,6 +16,7 @@ function fetchSystemFonts(): Promise<string[]> {
     // Given I coded both interfaces to match the definition above, I don't
     // need to continue calling platform.select
     return NativeFontLookup.getFontFamilies()
+        .then((fonts) => fonts.sort((a, b) => a.localeCompare(b)));
 }
 
 export { NativeFontLookup, fetchSystemFonts };
