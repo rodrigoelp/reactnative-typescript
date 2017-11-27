f94f0ffb0b3b77dbf70f78b465c4700684148b45
Rodrigo Landaeta <rlandaeta@cochlear.com>
2017-11-28 09:04:41 +1100

    Created the connected component, although I am not using anything of redux so far

--- samples/13.tabbedredux/src/appShell.tsx
+++ samples/13.tabbedredux/src/appShell.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { View, Button } from "react-native";
-import { Provider } from "react-redux";
+import { Provider, connect } from "react-redux";
 import { StackNavigator, NavigationRouteConfigMap, StackNavigatorConfig } from "react-navigation";
 import { HostRouteNames } from "./models";
 import { AppTabNavigator } from "./appScreens";
@@ -30,6 +30,22 @@ const stackConfig: StackNavigatorConfig = {
 const AppNavigator = StackNavigator(routeConfig, stackConfig);
 
 /**
+ * This container will provide a mechanism to communicate from other containers
+ * which screen does the navigator should display and provide its state.
+ */
+class AppNavigatorContainer extends React.Component {
+    constructor(props: any) {
+        super(props);
+    }
+
+    public render() {
+        return <AppNavigator />;
+    }
+}
+
+const ConnectedAppNavigatorContainer = connect()(AppNavigatorContainer);
+
+/**
  * This is the shell of the application, basically hosts the provider that will be manipulating the navigator
  * via a smart component (or container)
  */
@@ -37,7 +53,7 @@ class AppShell extends React.PureComponent {
     public render() { // this will provide the entry point for redux to start doing its thing.
         return (
             <Provider store={store}> 
-                <AppNavigator />
+                <ConnectedAppNavigatorContainer />
             </Provider>
         )
     }
