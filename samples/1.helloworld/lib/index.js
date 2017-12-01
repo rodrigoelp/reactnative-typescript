539b958a5147a3c057eb426fe7d92e03d72c8927
Rodrigo Landaeta <rlandaeta@cochlear.com>
2017-12-01 22:19:54 +1100

    Removing exercise 1 in prep to add the git submodule

--- samples/1.helloworld/lib/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const react_native_1 = require("react-native");
-const App_1 = require("./App");
-react_native_1.AppRegistry.registerComponent("helloworld", () => App_1.default);
-//# sourceMappingURL=index.js.map
