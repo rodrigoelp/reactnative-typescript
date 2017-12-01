10f4a70f225a44fbbe8fe0b1b15380a2573b6739
Rodrigo Landaeta <rlandaeta@cochlear.com>
2017-12-02 09:20:03 +1100

    Removing the exercise 3 folder in prep to add it back as a submodule

--- samples/3.ToDos/src/index.tsx
+++
@@ -1,5 +0,0 @@
-import * as React from "react";
-import { AppRegistry } from "react-native";
-import { AppNavigator } from "./components/AppNavigator";
-
-AppRegistry.registerComponent("ToDo", () => AppNavigator);
