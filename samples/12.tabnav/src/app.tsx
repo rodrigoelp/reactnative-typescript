34aa5498af561167410e3cb0904a73e2f3d24e52
Rodrigo Landaeta <rlandaeta@cochlear.com>
2017-12-04 12:14:52 +1100

    Removed exercise 12 in preparation to move it to an external repo.

--- samples/12.tabnav/src/app.tsx
+++
@@ -1,5 +0,0 @@
-import * as React from "react";
-import { AppRegistry, View, Text } from "react-native";
-import { AppShell } from "./screens";
-
-AppRegistry.registerComponent("tabnav", () => AppShell);
