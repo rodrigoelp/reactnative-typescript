4f14d14ac8da351868a1bd867f08689f96bfa2c5
Rodrigo Landaeta <rlandaeta@cochlear.com>
2017-12-04 12:14:21 +1100

    Removed exercise 7 in preparation to move it to an external repo.

--- samples/7.counterredux/src/containers/index.ts
+++
@@ -1,4 +0,0 @@
-
-import CounterApp from "./CounterApp";
-
-export { CounterApp };
--- samples/7.counterredux/src/store.ts
+++
@@ -1,5 +0,0 @@
-
-import { createStore } from "redux";
-import { allReducer } from "./reducers/index";
-
-export const store = createStore(allReducer);
