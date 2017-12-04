9d2bd4e5736ea76047924294f185395ea289992f
Rodrigo Landaeta <rlandaeta@cochlear.com>
2017-12-04 12:14:15 +1100

    Removed exercise 6 in preparation to move it to an external repo.

--- samples/6.simpleredux/src/models/index.ts
+++
@@ -1,4 +0,0 @@
-import { Game, GameScore, Gender, ScreenName, User, genderToAdjective } from "./models";
-import { UniqueId } from "./sysTypes";
-
-export { Game, GameScore, Gender, ScreenName, User, genderToAdjective, UniqueId };
